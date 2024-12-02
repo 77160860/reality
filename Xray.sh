5bd46ca825c140b070b3f09779e81b2f3f738f37
77160860 <77160860@qq.com>
2024-12-02 21:01:18 +0800

    Update Xray.sh

--- Xray.sh
+++ Xray.sh
@@ -11,7 +11,7 @@ RESET='\033[0m'
 
 # 函数用于安装 Xray
 install_vless_reality() {
-   bash <(curl -fsSL https://raw.githubusercontent.com/77160860/Xray/refs/heads/main/xray.sh)
+   bash <(curl -fsSL https://raw.githubusercontent.com/77160860/reality/refs/heads/main/xray.sh)
 }
 
 # 函数用于卸载 Xray
